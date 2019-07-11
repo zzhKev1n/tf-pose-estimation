b47208b171eaa626a0b7cbedb00e6cfa0df144da
Aiden Ray <aidenunsw@outlook.com>
2019-07-11 17:54:11 +1000

    fix fps text

--- src/assignment.py
+++ src/assignment.py
@@ -103,10 +103,10 @@ if __name__ == '__main__':
 
         # drawing lines on an image
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
-            hail_taxi(image)
+
         # FPS counter
-        cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)),
-                    (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.putText(image, "FPS: {:.2f}".format(1.0 / (time.time() - fps_time)),
+                    (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 0, 0), 2)
         cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
         if cv2.waitKey(1) == 27:
